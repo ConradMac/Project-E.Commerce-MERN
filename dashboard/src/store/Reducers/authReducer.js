67eaeac331704c149df08f2a2807b2e5b2a79a4d
Conrad 宮文軒 <mackayleblanc.conrad@gmail.com>
2024-04-15 16:19:15 +0200

    modified authReducer file

--- dashboard/src/store/Reducers/authReducer.js
+++ dashboard/src/store/Reducers/authReducer.js
@@ -3,8 +3,8 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const adminLogin = createAsyncThunk("auth/admin_login", async (info) => {
     console.log(info);
     try {
-        // const { data } = await api.post("admin/login", info, { withCredentials: true });
-        // console.log(data);
+        const { data } = await api.post("/admin/login", info, { withCredentials: true });
+        console.log(data);
     } catch (cons) {}
 });
 
