9678dc61ec6e1ffb08e8702526532da1ee3bab76
Conrad 宮文軒 <mackayleblanc.conrad@gmail.com>
2024-04-25 11:07:27 +0200

    Error message displayed on the screen with messageClear, useEffect(), exported messageClear, ad declared messagClear

--- dashboard/src/store/Reducers/authReducer.js
+++ dashboard/src/store/Reducers/authReducer.js
@@ -2,15 +2,17 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "./../../api/api";
 
 // createAsyncThunk takes two arguments: the first argument is the name of the action, and the second argument is an async function that will be executed when the action is dispatched. The async function takes two arguments: the first argument is the payload that was passed to the action, and the second argument is an object that contains information about the action, such as the dispatch function and the getState function.
-export const admin_Login = createAsyncThunk("auth/admin_login", async (info) => {
+export const admin_Login = createAsyncThunk("auth/admin_login", async (info, { rejectWithValue, fulfillWithValue }) => {
     console.log(info);
     try {
         // ici, je veux passer les informations de l'utilisateur au backend pour qu'il puisse les vérifier. data est un nomé donné pour toutes les datas qui seront envoyées au backend.
         const { data } = await api.post("/admin-login", info, { withCredentials: true });
         console.log(data);
+        return fulfillWithValue(data);
     } catch (error) {
         // cette ligne fait reference a notre fichier response dans le backend.
         console.log(error.response.data);
+        return rejectWithValue(error.response.data);
     }
 });
 
@@ -20,15 +22,27 @@ export const authReducer = createSlice({
         // On fait apparaître les messages d'erreur et de succès, ainsi que le loader et les informations de l'utilisateur grave a hot-toast que l'on a installé.
         successMessage: "",
         errorMessage: "",
-        loader: "false",
+        loader: false, // Utilisez false plutôt que "false"
         userInfo: "",
     },
-    reducer: {},
+    reducers: {
+        // Définition de l'action messageClear pour effacer le message d'erreur
+        messageClear: (state, _) => {
+            state.errorMessage = "";
+        },
+    },
     extraReducers: (builder) => {
-        builder.addCase(admin_Login.pending, (state, { payload }) => {
-            state.loader = true;
-        });
+        // On ajoute un gestionnaire pour l'action admin_Login.pending. L'action admin_Login.pending est automatiquement déclenchée par Redux Toolkit lorsqu'une action asynchrone commence à s'exécuter. Dans ce cas, nous voulons simplement définir loader sur true pour afficher le spinner de chargement.
+        builder
+            .addCase(admin_Login.pending, (state, { payload }) => {
+                state.loader = true;
+            })
+            .addCase(admin_Login.rejected, (state, { payload }) => {
+                state.loader = false;
+                state.errorMessage = payload.error;
+            });
     },
 });
-
+// Exportation de l'action messageClear depuis la tranche Redux authReducer
+export const { messageClear } = authReducer.actions;
 export default authReducer.reducer;
--- dashboard/src/views/auth/AdminLogin.jsx
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { admin_Login } from "../../store/Reducers/authReducer";
-
+import { admin_Login, messageClear } from "../../store/Reducers/authReducer";
 //  pour le loader
 import { PropagateLoader } from "react-spinners";
+import toast from "react-hot-toast";
 
 function AdminLogin() {
     const dispatch = useDispatch();
 
     // Cette ligne utilise le hook useSelector fourni par React Redux pour extraire la valeur de loader à partir du state global. dans authReducer
-    const { loader } = useSelector((state) => state.auth);
+    const { loader, errorMessage } = useSelector((state) => state.auth);
 
     const [state, setState] = useState({
         email: "",
@@ -28,6 +28,23 @@ function AdminLogin() {
         dispatch(admin_Login(state));
         // console.log(state);
     };
+
+    const overrideStyle = {
+        display: "flex",
+        margin: "0 auto",
+        height: "24px",
+        justifyContent: "center",
+        alignItems: "center",
+    };
+
+    // Ce useEffect est utilisé pour afficher les messages d'erreur et de succès à l'aide de la bibliothèque hot-toast. Si errorMessage est défini, cela signifie qu'une erreur s'est produite lors de la tentative de connexion de l'utilisateur, et nous affichons un message d'erreur à l'aide de la fonction error de hot-toast.
+    // Utilisation de useEffect pour afficher et effacer le message d'erreur
+    useEffect(() => {
+        if (errorMessage) {
+            toast.error(errorMessage); // Affichage du message d'erreur avec toast
+            dispatch(messageClear()); // Appel à l'action messageClear pour effacer le message d'erreur
+        }
+    }, [errorMessage]); // Dépendance : le message d'erreur
 
     return (
         <div className="min-w-screen min-h-screen bg-[#2cdcae9] flex justify-center items-center ">
@@ -72,10 +89,10 @@ function AdminLogin() {
                         </div>
 
                         <button
-                            // disabled={loader ? true : false}
+                            disabled={loader} // Pas besoin de ternaire ici
                             className="bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px700 py-2 mb-3"
                         >
-                            Log In
+                            {loader ? <PropagateLoader color="#fff" cssOverride={overrideStyle} /> : "Login"}
                         </button>
                     </form>
                 </div>
