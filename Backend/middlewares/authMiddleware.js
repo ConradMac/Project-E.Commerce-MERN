d36b7532b5b07313f084071ef77707a96f25b314
Conrad 宮文軒 <mackayleblanc.conrad@gmail.com>
2024-04-30 20:04:21 +0200

    created middlewares

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -1,9 +1,25 @@
 const jwt = require("jsonwebtoken");
+// Importation de la dépendance jsonwebtoken depuis le fichier package.json
 
 module.exports.authMiddleware = async (req, res, next) => {
-    const { accessToken } = req.cookies;
+    // Récupération du token de l'utilisateur depuis les cookies, stocké précédemment dans authControllers.js
+    const { accessToken } = req.cookies; // Extraction du cookie accessToken de la requête
 
+    // Si le token n'est pas présent, renvoi d'une erreur et arrêt de l'exécution du code
     if (!accessToken) {
         return res.status(409).json({ error: "Please Login First" });
+    } else {
+        try {
+            // Vérification et décodage du token à l'aide de la clé secrète stockée dans les variables d'environnement
+            const deCodeToken = await jwt.verify(accessToken, process.env.SECRET_KEY);
+            // Stockage du rôle et de l'ID de l'utilisateur dans la requête pour une utilisation ultérieure
+            req.role = deCodeToken.role;
+            req.id = deCodeToken.id;
+            // Appel de la fonction next() pour passer au middleware suivant dans la pile
+            next();
+        } catch (error) {
+            // En cas d'erreur lors de la vérification du token, renvoi d'une erreur et arrêt de l'exécution du code
+            return res.status(409).json({ error: "Please Login First" });
+        }
     }
 };
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,3 +1,4 @@
+const { authMiddleware } = require("../middlewares/authMiddleware");
 const authControllers = require("./../controllers/authControllers");
 
 // nous créerons une route pour la fonction admin_login
@@ -5,5 +6,7 @@ const router = require("express").Router();
 
 // nous créerons une route pour la fonction admin_login qui apres le authControllers.admin_login sera use dans le authControllers.js
 router.post("/admin-login", authControllers.admin_login);
+
+router.get("get-user", authMiddleware, authControllers.getUser);
 
 module.exports = router;
