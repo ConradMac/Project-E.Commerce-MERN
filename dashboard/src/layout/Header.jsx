491983cd64e8e1e32f320fe5c402a00788cfffd9
Conrad 宮文軒 <mackayleblanc.conrad@gmail.com>
2024-05-19 15:24:39 +0200

    Header design completed

--- dashboard/src/layout/Header.jsx
+++ dashboard/src/layout/Header.jsx
@@ -4,10 +4,10 @@ import { FaList } from "react-icons/fa";
 function Header({ showSideBar, setShowSideBar }) {
     return (
         <div className="fixed top-0 left-0 w-full py-5 px-2 lg:px-7 z-40">
-            <div className="ml-0 lg:ml-[260] rounded-md h-[65px] flex justify-between items-center bg-[#b1addf] px-5 transition-all">
+            <div className="ml-0 lg:ml-[260px] rounded-md h-[65px] flex justify-between items-center bg-[#b1addf] px-5 transition-all">
                 <div
                     onClick={() => setShowSideBar(!showSideBar)}
-                    className="w-[35px] flex lg:hidden h-[35px] rounded-sm bg-indigo-500 shadow-lg hover:shadow-indigo-500/50 justify-center items-center cursor-pointer"
+                    className="w-[35px] flex lg:hidden h-[35px] rounded-sm bg-indigo-500 shadow-lg hover:shadow-indigo-500/50 justify-center items-center cursor-pointer "
                 >
                     <span>
                         <FaList />
@@ -16,11 +16,27 @@ function Header({ showSideBar, setShowSideBar }) {
 
                 <div className="hidden md:block">
                     <input
-                        className="px-3 py-2 outline-none border bg-transparent border-slate-700 rounded-md text-[#cdcae9] focus:border-indico-500 overflow-hidden "
+                        className="px-3 py-2 outline-none border bg-transparent border-slate-700 rounded-md text-[#423d72] focus:border-indigo-300 overflow-hidden"
                         type="text"
                         name="search"
-                        placeholder="Search..."
+                        placeholder="search"
                     />
+                </div>
+
+                <div className="flex justify-center items-center gap-8 relative ">
+                    <div className="flex justify-center items-center">
+                        <div className="flex justify-center items-center gap-3">
+                            <div className="flex justify-center items-center flex-col text-end">
+                                <h2 className="text-md font-bold">Conrad LEBLANC</h2>
+                                <span className="text-[14px] w-full font-normal">Admin</span>
+                            </div>
+                            <img
+                                className="w-[45px] h-[45px] rounded-full hoverflow-hidden"
+                                src="http://localhost:3000/images/admin.jpg"
+                                alt=""
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
