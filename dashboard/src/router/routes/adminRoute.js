5a31f485dd9694621132fcbef17ff49a9620003e
Conrad 宮文軒 <mackayleblanc.conrad@gmail.com>
2024-05-28 11:32:53 +0200

    category page design completed and sellers page completed

--- dashboard/src/router/routes/adminRoute.js
+++ dashboard/src/router/routes/adminRoute.js
@@ -1,7 +1,10 @@
 import { lazy } from "react";
 
+const Category = lazy(() => import("../../views/admin/Category"));
+
 const AdminDashboard = lazy(() => import("../../views/admin/AdminDashboard"));
 const Orders = lazy(() => import("../../views/admin/Orders"));
+const Sellers = lazy(() => import("../../views/admin/Sellers"));
 
 export const adminRoutes = [
     {
@@ -13,10 +16,34 @@ export const adminRoutes = [
         role: "admin",
     },
     {
-        // Chemin de la route
+        // Chemin de la route order
         path: "admin/dashboard/orders",
 
         element: <Orders />,
+
+        role: "admin",
+    },
+    {
+        // Chemin de la route catgory
+        path: "admin/dashboard/category",
+
+        element: <Category />,
+
+        role: "admin",
+    },
+    {
+        // Chemin de la route sellers
+        path: "admin/dashboard/sellers",
+
+        element: <Sellers />,
+
+        role: "admin",
+    },
+    {
+        // Chemin de la route sellers
+        path: "admin/dashboard/sellers",
+
+        element: <Sellers />,
 
         role: "admin",
     },
